077b1c49aed34f9f4fdaccfe4a9e0fde69367214
Webmaster444 <jlmobile710@gmail.com>
2017-11-07 08:21:26 +0100

    draw all nodes from json

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,48 +4,211 @@ var svg = d3.select("#content").append("svg")
     .append("g")
     .attr("transform", "translate(30,10)");
 
-drawRotatedRoundSquare(svg,40,30,30,'black',1);
-drawRect(svg,80,30,30,40,'black',1);
-drawRoundRect(svg,120,30,30,40,'black',1);
-drawArrow(1,1,30,10,5);
+var jsondata = [{
+  "items": {
+    "1": {
+      "title": "Buyer receives payment notification",
+      "id": 1,
+      "description": "",
+      "type": "start",
+      "stream": 1,
+      "attachment": "http://upload1.location",
+      "connectors": {
+        "1": {
+          "title": "",
+          "type": "simple",
+          "linkTo": 2
+        }
+      }
+    },
+    "2": {
+      "title": "",
+      "id": 2,
+      "description": "",
+      "type": "junction",
+      "stream": 1,
+      "attachment": "http://upload1.location",
+      "connectors": {
+        "1": {
+          "title": "",
+          "type": "simple",
+          "linkTo": 3
+        }
+      }
+    },
+    "3": {
+      "title": "Fully Paid?",
+      "id": 3,
+      "description": "",
+      "type": "decision",
+      "stream": 2,
+      "attachment": "http://upload1.location",
+      "connectors": {
+        "1": {
+          "title": "Connector 5 Name",
+          "type": "simple",
+          "linkTo": 4
+        },
+        "2": {
+          "title": "Connector 4 Name",
+          "type": "simple",
+          "linkTo": 5
+        }
+      }
+    },
+    "4": {
+      "title": "Send Invoice to Buyer",
+      "id": 4,
+      "description": "",
+      "type": "process-simple",
+      "stream": 1,
+      "attachment": "http://upload1.location",
+      "connectors": {
+        "1": {
+          "title": "",
+          "type": "simple",
+          "linkTo": 6
+        }
+      }
+    },
+    "5": {
+      "title": "Email buyer ask for full payment",
+      "id": 5,
+      "description": "",
+      "type": "process-simple",
+      "stream": 1,
+      "attachment": "http://upload1.location",
+      "connectors": {
+        "1": {
+          "title": "",
+          "type": "simple",
+          "linkTo": 2
+        }
+      }
+    },
+    "6": {
+      "title": "Send Shipment notice to Buyer",
+      "id": 6,
+      "description": "",
+      "type": "finish",
+      "stream": 1,
+      "attachment": "http://upload1.location",
+      "connectors": {}
+    }
+  },
+  "streams": {
+    "1": {
+      "id": 1,
+      "title": "HR Staff",
+      "order": "1"
+    }
+  }
+}];
 
-rect = svg.append("path")
-        .attr("d", drawArrow(0, 100, 100, 0, 3))
+var defColor = '#337ab7';
+var priColor = '#f0ad4e';
+for(index in jsondata[0].items){    
+    var item = jsondata[0].items[index];
+    if(item.type=='start'){
+        drawRoundRect(svg,30,30,120,40,item.title,defColor);
+    }
+    if(item.type=='junction'){
+        drawJunctionOperator(svg,200,30,defColor,15);
+    }
+
+    if(item.type=='decision'){
+        drawRotatedRoundSquare(svg,260,30,60,defColor,5,item.title);
+    }
+
+    if(item.type=='process-simple'&& item.id==4){        
+        drawRect(svg,440,30,120,40,defColor,item.title);
+    }
+
+    if(item.type=='process-simple'&& item.id==5){        
+        drawRect(svg,280,130,120,40,defColor,item.title);
+    }
+
+    if(item.type=='finish'){        
+        drawRoundRect(svg,640,30,120,40,item.title,priColor);
+    }
+}
+// jsondata.items.each(function(d){
+
+// })
 
 //draw rectangle
-function drawRect(container, x,y, width, height,color){
-    var g = container.append('g').attr('class','g_wrapper');
-    return g.append("rect")
-        .attr("x", x)
-        .attr("y", y)
+function drawRect(container, x,y, width, height,color,text){
+    var g = container.append('g').attr('class','g_wrapper').attr('transform',function(){
+        return "translate("+x+","+y+")";        
+    });;
+    var res = g.append("rect")
+        .attr("x", 0)
+        .attr("y", 0)
         .attr("width", width)
         .attr("height", height)
         .attr('stroke', color);    
+
+    res += g.append("text").text(text).attr('x',width/2).attr('y',height/2).attr('text-anchor','middle').attr('dy','.1em').call(wrap,width);
 }
 
 //draw rounded rectangle
-function drawRoundRect(container, x, y, width, height, color, rx){
-    var g = container.append('g').attr('class','g_wrapper');
-    return g.append("rect")
+function drawRoundRect(container, x, y, width, height, text,color, rx){
+    if(rx==''||rx==undefined){
+        rx = 3;
+    }
+    var g = container.append('g').attr('class','g_wrapper').attr('transform',function(){
+        return "translate("+x+","+y+")";        
+    });
+    var res =  g.append("rect")
         .attr('ry',rx)
-        .attr("x", x)
-        .attr("y", y)
+        .attr("x", 0)
+        .attr("y", 0)
         .attr("width", width)
         .attr("height", height)
         .attr('stroke', color);   
+    res += g.append("text").text(text).attr('x',width/2).attr('y',height/2).attr('text-anchor','middle').attr('dy','.1em').call(wrap,width);
+
 }
 
 //draw round square and rotate
-function drawRotatedRoundSquare(container, x, y, width, color, rx){
-    var g = container.append('g').attr('class','g_wrapper');
-    return g.append("rect")
+function drawRotatedRoundSquare(container, x, y, width, color, rx,text){
+    var g = container.append('g').attr('class','g_wrapper').attr('transform',function(){
+        return "translate("+x+","+y+")";        
+    });
+    var res =  g.append("rect")
         .attr('ry',rx)
-        .attr("x", x)
-        .attr("y", y)
+        .attr("x", 0)
+        .attr("y", 0)
         .attr("width", width)
         .attr("height", width)
         .attr('stroke', color)
-        .attr('transform','rotate(45)');   
+        .attr('transform','translate(80, -30) rotate(45)');   
+    res += g.append("text").text(text).attr('x',(width+95)/2).attr('y',(width/2 -15)).attr('text-anchor','middle').attr('dy','.1em').call(wrap,width);
+    return res;
+}
+
+function drawJunctionOperator(container, x, y, color, rx){
+    var g = container.append('g').attr('class','g_wrapper');
+    g.append("circle")
+        .attr('r',rx)
+        .attr("cx", (x+rx))
+        .attr("cy", (y+rx))        
+        .attr('stroke', color);        
+    g.append('line')
+        .attr('x1',x+5)
+        .attr('y1',y+5)
+        .attr('x2',(x+2*rx -5))
+        .attr('y2',(y+2*rx - 5))
+        .attr('stroke',defColor)
+        .attr('stroke-width','3px');
+
+    g.append('line')
+        .attr('x1',x+5)
+        .attr('y1',(y+2*rx - 5))
+        .attr('x2',(x+2*rx -5))
+        .attr('y2',y+5)
+        .attr('stroke',defColor)
+        .attr('stroke-width','3px');
 }
 //draw arrow
 function drawArrow(x, y, width, height, ahwidth) {
@@ -68,8 +231,9 @@ function wrap(text, width) {
         lineNumber = 0,
         lineHeight = 1.1, // ems
         y = text.attr("y"),
+        x = text.attr('x'),
         dy = parseFloat(text.attr("dy")),
-        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+        tspan = text.text(null).append("tspan").attr("x", x).attr("y", y).attr("dy", dy + "em");
     while (word = words.pop()) {
       line.push(word);
       tspan.text(line.join(" "));
@@ -77,7 +241,7 @@ function wrap(text, width) {
         line.pop();
         tspan.text(line.join(" "));
         line = [word];
-        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+        tspan = text.append("tspan").attr("x", x).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
       }
     }
   });
